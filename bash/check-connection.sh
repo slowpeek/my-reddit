34c0d9b9ad481aec929be74fc847270fb238087e
slowpeek <slowpeek@users.noreply.github.com>
2021-06-29 21:57:01 +0300

    init

---
+++ bash/check-connection.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Author: /u/kevors
+# Reddit: https://www.reddit.com/r/bash/comments/o8go3x/:/h359gkt/
+
+t_rst=$(tput sgr0)
+t_info=$(tput setaf 2)
+t_warn=$(tput setaf 3)
+t_err=$(tput setaf 1)
+t_crdl=$(tput -S <<< $'cr\ndl1')
+
+ghome=google.com
+gdns=8.8.8.8
+
+read -r gway gifc < <(ip r | awk '/default/ { print $3, $5 }')
+
+readarray -td@ text <<EOF
+
+You are ${t_info}connected${t_rst} to those your internets via interface ${gifc}.
+
+@
+
+Your ${t_warn}DNS server(s)${t_rst} does not seem to be responding.
+/etc/resolv.conf file might be of interest to you.
+
+@
+
+Problem seems to be with the ${t_warn}ISP${t_rst}. Contact the ISP.
+
+@
+
+${t_err}Can't connect${t_rst} to either your router or your modem.
+
+- Make sure the PC's network is properly configured.
+- Check if the router or modem is switched on.
+- Check if the network cable is correctly attached.
+- In case of wireless router check the signal level.
+
+EOF
+
+ping_test () {
+    echo -n ping "$1" ...
+
+    local st
+    ping -q -w1 -c1 "$1" &>/dev/null
+    st=$?
+
+    echo -n "$t_crdl"
+
+    return "$st"
+}
+
+say () {
+    local s=${text[$1]}
+
+    s=${s#${s%%[![:space:]]*}}
+    s=${s%${s##*[![:space:]]}}
+
+    printf '\n%s\n\n' "$s"
+}
+
+st=0 n=0
+for host in ghome gdns gway; do
+    if ping_test "${!host}"; then
+        say "$n"
+        exit "$st"
+    fi
+
+    st=1
+    ((++n))
+done
+
+say "$n"
+exit 1
---
+++ bash/file-selector.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Author: /u/kevors
+# Reddit: https://www.reddit.com/r/bash/comments/oa7srl/:/h3gda9u/
+
+file_selector() {
+    local origin=${1%${1##*[^/]}} menu=() sel up
+    local cwd=$origin PS3
+
+    # Reset result
+    FILE_SELECTOR_RESULT=
+
+    while true; do
+        clear -x
+	    printf '%s\n' "${cwd:-/}"
+
+        PS3=$'-----\nq Quit'
+
+        up=n
+        # Restrict the user to the original $1
+        if [[ $cwd != "$origin" ]]; then
+            up=y
+            PS3+='    u Up'
+        fi
+
+        PS3+=$'\n-----\nPlease, select: '
+
+        # Test for empty dir
+        if read -r < <(find "${cwd:-/}" -maxdepth 0 -type d ! -empty); then
+            readarray -t menu < <(find "$cwd"/* -maxdepth 0 -printf '%y%f%y\n' |\
+                                      sed 's~^[^d]~z~;s~[^d]$~~;s~d$~/~' | sort | cut -c2-)
+
+            printf '%d items\n\n' "${#menu[@]}"
+
+	        select sel in "${menu[@]}"; do
+                break
+            done
+        else
+            read -r -p $'empty\n\n'"$PS3"
+            sel=
+        fi
+
+        case $REPLY in
+            [qQ])
+                # Quit, nothing selected
+                return 1
+                ;;
+            [uU])
+                # Go up if allowed
+                [[ $up == n ]] || cwd=${cwd%/*}
+                ;;
+            *)
+                case $sel in
+                    '')
+                    ;;
+                    */)
+                        # Dir selected, descend
+			            cwd+=/"${sel::-1}"
+                        ;;
+                    *)
+                        # Not dir selected, return the selection
+			            FILE_SELECTOR_RESULT=$sel
+                        return 0
+                        ;;
+                esac
+        esac
+    done
+}
+
+
+file_selector ~
+echo "$FILE_SELECTOR_RESULT"
